9156ebdf07e95fecd7583dd83886682d4775fa16
Danilo2505 <77727610+Danilo2505@users.noreply.github.com>
2025-11-10 09:05:57 -0300

    Aula 03 (11/11/2025)

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,11 @@
+const buttonMenu = document.getElementById("button-menu-toggle");
+const headerTopBar = document.getElementById("header-top-bar");
+
+buttonMenu.addEventListener("click", () => {
+  headerTopBar.classList.toggle("menu-open");
+});
+
+/*
 const buttonMenuToggle = document.querySelector("#button-menu-toggle");
 
 buttonMenuToggle.addEventListener("click", () => {
@@ -7,3 +15,4 @@ buttonMenuToggle.addEventListener("click", () => {
     buttonMenuToggle.setAttribute("close", "");
   }
 });
+*/
